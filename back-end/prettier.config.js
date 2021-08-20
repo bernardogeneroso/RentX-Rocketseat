bcde81e6091a87138f5928448d342c852889aa7f
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-08-20 14:58:29 +0100

    Start creating strecture back-end

---
+++ back-end/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  arrowParams: "avoid",
+  trailingComma: "none",
+  endOfLine: "auto",
+};
---
+++ back-end/src/shared/infra/http/server.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata"
+import "dotenv/config"
+import "express-async-errors"
+
+import express, { Request, Response, NextFunction } from "express"
+import cors from "cors"
+import { errors } from "celebrate"
+
+import AppError from "../../errors/AppError"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(errors())
+
+app.use(
+  (error: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).send({
+        status: "error",
+        message: error.message
+      })
+    }
+
+    if (error.message === "File too large") {
+      return response.status(400).send({
+        status: "error",
+        message: "Maximum file size is 10MB"
+      })
+    }
+
+    console.log(error.message) /* eslint-disable-line */
+
+    return response.status(500).send({
+      status: "error",
+      message: "Internal server error"
+    })
+  }
+)
+
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log(`🚀 Server started on port ${port}`)
+})
