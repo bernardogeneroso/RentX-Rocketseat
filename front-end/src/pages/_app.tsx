00c2725d0ac7f2f31cd4ed78d649f58f8905f616
Bernardo Generoso <generoso.bernardoapple@gmail.com>
2021-10-12 18:27:02 +0100

    Add progress bar on load page

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 import { ThemeProvider } from 'styled-components'
 
 import light from '../styles/themes/light'
@@ -9,14 +12,35 @@ import 'react-calendar/dist/Calendar.css'
 import '@reach/dialog/styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleStart = (url: any) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
-    <>
+    <ThemeProvider theme={light}>
       <GlobalStyle />
 
-      <ThemeProvider theme={light}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 export default MyApp
--- front-end/src/styles/global.ts
+++ front-end/src/styles/global.ts
@@ -58,4 +58,74 @@ export default createGlobalStyle`
       padding: 2rem;
     }
   }
+
+  #nprogress {
+    pointer-events: none;
+  }
+
+  #nprogress .bar {
+    background: ${(props) => props.theme.colors.primary};
+
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 4px;
+  }
+
+  #nprogress .peg {
+    display: block;
+    position: absolute;
+    right: 0px;
+    width: 100px;
+    height: 100%;
+    box-shadow: 0 0 10px ${(props) => props.theme.colors.primary}, 0 0 5px ${(
+  props
+) => props.theme.colors.primary};
+    opacity: 1;
+
+    transform: rotate(3deg) translate(0px, -4px);
+  }
+
+  #nprogress .spinner {
+    display: block;
+    position: fixed;
+    z-index: 1031;
+    top: 15px;
+    right: 15px;
+  }
+
+  #nprogress .spinner-icon {
+    width: 18px;
+    height: 18px;
+    box-sizing: border-box;
+
+    border: solid 2px transparent;
+    border-top-color: ${(props) => props.theme.colors.primary};
+    border-left-color: ${(props) => props.theme.colors.primary};
+    border-radius: 50%;
+
+    animation: nprogress-spinner 400ms linear infinite;
+  }
+
+  .nprogress-custom-parent {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .nprogress-custom-parent #nprogress .spinner,
+  .nprogress-custom-parent #nprogress .bar {
+    position: absolute;
+  }
+
+  @keyframes nprogress-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `
